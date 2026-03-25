5d607c6a4114c9a2605f83cd9144c61cfaa8e77f
ElGatoPro300 <catgamer207@gmail.com>
2026-03-24 23:07:21 -0600

    1.21.1 files

--- src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/widgets/UICreativeItemSelectorPanel.java
+++ src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/widgets/UICreativeItemSelectorPanel.java
@@ -19,6 +19,8 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.NbtElement;
 import net.minecraft.registry.Registries;
 
 import java.util.ArrayList;
@@ -183,7 +185,10 @@ public class UICreativeItemSelectorPanel extends UIOverlayPanel
             return;
         }
 
-        String key = Registries.ITEM.getId(stack.getItem()) + "|" + stack.getNbt();
+        ItemStack normalized = stack.copy();
+        normalized.setCount(1);
+        String encoded = ItemStack.CODEC.encodeStart(NbtOps.INSTANCE, normalized).result().map(NbtElement::asString).orElse("{}");
+        String key = Registries.ITEM.getId(stack.getItem()) + "|" + encoded;
 
         if (visited.add(key))
         {
