8b12589425637a55c183b8e07f2805fea29383e4
ElGatoPro300 <catgamer207@gmail.com>
2026-03-31 20:06:20 -0600

    1.21.1 files

--- src/main/java/mchorse/bbs_mod/mixin/WorldMixin.java
+++ src/main/java/mchorse/bbs_mod/mixin/WorldMixin.java
@@ -2,6 +2,7 @@ package mchorse.bbs_mod.mixin;
 
 import mchorse.bbs_mod.BBSMod;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -13,8 +14,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(World.class)
 public class WorldMixin
 {
-    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z", at = @At("HEAD"))
-    public void onSetBlockState(BlockPos pos, BlockState state, int flags, int maxUpdateDepth, CallbackInfoReturnable<Boolean> info)
+    @Inject(method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z", at = @At("HEAD"), require = 0)
+    public void onSetBlockStateThreeArgs(BlockPos pos, BlockState state, int flags, CallbackInfoReturnable<Boolean> info)
+    {
+        this.captureBeforeSetBlockState(pos);
+    }
+
+    @Inject(method = "breakBlock(Lnet/minecraft/util/math/BlockPos;ZLnet/minecraft/entity/Entity;I)Z", at = @At("HEAD"), require = 0)
+    public void onBreakBlockFourArgs(BlockPos pos, boolean drop, Entity breakingEntity, int maxUpdateDepth, CallbackInfoReturnable<Boolean> info)
+    {
+        this.captureBeforeSetBlockState(pos);
+    }
+
+    private void captureBeforeSetBlockState(BlockPos pos)
     {
         if ((Object) this instanceof ServerWorld world)
         {
