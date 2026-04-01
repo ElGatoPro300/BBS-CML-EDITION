981313fdaaf9f9644137c883fc87d7a57e90de3a
ElGatoPro300 <catgamer207@gmail.com>
2026-03-31 19:52:21 -0600

    Revert "1.21.4 files"
    This reverts commit 7544a9725badee601a42dd72283902d8a6085421.

--- src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/shape/UIShapeNodeEditor.java
+++ src/client/java/mchorse/bbs_mod/ui/forms/editors/panels/shape/UIShapeNodeEditor.java
@@ -51,7 +51,6 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexFormat;
 import net.minecraft.client.render.VertexFormats;
-import net.minecraft.client.gl.ShaderProgramKeys;
 import org.joml.Matrix4f;
 import org.joml.Vector2f;
 import org.lwjgl.glfw.GLFW;
@@ -1160,7 +1159,7 @@ public class UIShapeNodeEditor extends UIElement
 
         RenderSystem.enableBlend();
         RenderSystem.defaultBlendFunc();
-        RenderSystem.setShader(ShaderProgramKeys.POSITION_COLOR);
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
 
         // Border
         BufferBuilder builder = tessellator.begin(VertexFormat.DrawMode.TRIANGLES, VertexFormats.POSITION_COLOR);
