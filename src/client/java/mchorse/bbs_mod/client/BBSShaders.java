e1753ab5d1a5768c8b2ebcfa854b4d5da899eeb2
ElGatoPro300 <catgamer207@gmail.com>
2026-04-05 18:38:03 -0600

    restore BBS shaders

--- src/client/java/mchorse/bbs_mod/client/BBSShaders.java
+++ src/client/java/mchorse/bbs_mod/client/BBSShaders.java
@@ -1,8 +1,17 @@
 package mchorse.bbs_mod.client;
 
+import mchorse.bbs_mod.BBSMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
+import net.minecraft.client.gl.ShaderLoader;
 import net.minecraft.client.gl.ShaderProgram;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.util.Identifier;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.reflect.Method;
 
 public class BBSShaders
 {
@@ -17,6 +26,10 @@ public class BBSShaders
     private static ShaderProgram pickerBillboardNoShading;
     private static ShaderProgram pickerParticles;
     private static ShaderProgram pickerModels;
+    
+    public static RenderPipeline MODEL_PIPELINE;
+    public static RenderPipeline MULTILINK_PIPELINE;
+    public static RenderPipeline SUBTITLES_PIPELINE;
 
     static
     {
@@ -35,14 +48,21 @@ public class BBSShaders
         if (pickerParticles != null) pickerParticles.close();
         if (pickerModels != null) pickerModels.close();
 
-        model = null;
-        multiLink = null;
-        subtitles = null;
-        pickerPreview = null;
-        pickerBillboard = null;
-        pickerBillboardNoShading = null;
-        pickerParticles = null;
-        pickerModels = null;
+        ShaderLoader loader = MinecraftClient.getInstance().getShaderLoader();
+
+        model = loadProgram(loader, "core/model", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+        multiLink = loadProgram(loader, "core/multilink", VertexFormats.POSITION_TEXTURE_COLOR);
+        subtitles = loadProgram(loader, "core/subtitles", VertexFormats.POSITION_TEXTURE_COLOR);
+
+        pickerPreview = loadProgram(loader, "core/picker_preview", VertexFormats.POSITION_TEXTURE_COLOR);
+        pickerBillboard = loadProgram(loader, "core/picker_billboard", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+        pickerBillboardNoShading = loadProgram(loader, "core/picker_billboard_no_shading", VertexFormats.POSITION_TEXTURE_LIGHT_COLOR);
+        pickerParticles = loadProgram(loader, "core/picker_particles", VertexFormats.POSITION_COLOR_TEXTURE_LIGHT);
+        pickerModels = loadProgram(loader, "core/picker_models", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+        
+        MODEL_PIPELINE = registerPipeline("pipeline/model", "core/model", VertexFormats.POSITION_COLOR_TEXTURE_OVERLAY_LIGHT_NORMAL);
+        MULTILINK_PIPELINE = registerPipeline("pipeline/multilink", "core/multilink", VertexFormats.POSITION_TEXTURE_COLOR);
+        SUBTITLES_PIPELINE = registerPipeline("pipeline/subtitles", "core/subtitles", VertexFormats.POSITION_TEXTURE_COLOR);
 
         for (Runnable runnable : LOADERS)
         {
@@ -90,4 +110,99 @@ public class BBSShaders
         return pickerModels;
     }
 
+    private static ShaderProgram loadProgram(ShaderLoader loader, String path, Object vertexFormat)
+    {
+        Identifier id = Identifier.of(BBSMod.MOD_ID, path);
+
+        try
+        {
+            Method m = loader.getClass().getMethod("getOrCreateProgram", Identifier.class, vertexFormat.getClass());
+            return (ShaderProgram) m.invoke(loader, id, vertexFormat);
+        }
+        catch (Exception ignored) {}
+
+        try
+        {
+            Method m = loader.getClass().getMethod("getOrCreateProgram", Identifier.class);
+            return (ShaderProgram) m.invoke(loader, id);
+        }
+        catch (Exception ignored) {}
+
+        return null;
+    }
+
+    private static RenderPipeline registerPipeline(String pipelinePath, String shaderPath, Object vertexFormat)
+    {
+        try
+        {
+            Class<?> renderPipelineClass = Class.forName("com.mojang.blaze3d.pipeline.RenderPipeline");
+            Object builder = renderPipelineClass.getMethod("builder").invoke(null);
+            builder = invokeBest(builder, "withLocation", Identifier.of(BBSMod.MOD_ID, pipelinePath));
+            builder = invokeBest(builder, "withVertexShader", Identifier.of(BBSMod.MOD_ID, shaderPath));
+            builder = invokeBest(builder, "withFragmentShader", Identifier.of(BBSMod.MOD_ID, shaderPath));
+
+            Object mode = null;
+            try
+            {
+                Class<?> modeClass = Class.forName("com.mojang.blaze3d.vertex.VertexFormat$DrawMode");
+                mode = Enum.valueOf((Class<Enum>) modeClass, "TRIANGLES");
+            }
+            catch (Exception ignored) {}
+
+            if (mode != null)
+            {
+                builder = invokeBest(builder, "withVertexFormat", vertexFormat, mode);
+            }
+            else
+            {
+                builder = invokeBest(builder, "withVertexFormat", vertexFormat);
+            }
+
+            Object pipeline = renderPipelineClass.getMethod("build").invoke(builder);
+            Method register = RenderPipelines.class.getMethod("register", renderPipelineClass);
+
+            return (RenderPipeline) register.invoke(null, pipeline);
+        }
+        catch (Exception ignored)
+        {
+            return null;
+        }
+    }
+
+    private static Object invokeBest(Object target, String methodName, Object... args) throws Exception
+    {
+        Method[] methods = target.getClass().getMethods();
+
+        for (Method method : methods)
+        {
+            if (!method.getName().equals(methodName))
+            {
+                continue;
+            }
+
+            Class<?>[] params = method.getParameterTypes();
+            if (params.length != args.length)
+            {
+                continue;
+            }
+
+            boolean ok = true;
+
+            for (int i = 0; i < params.length; i++)
+            {
+                if (args[i] != null && !params[i].isAssignableFrom(args[i].getClass()))
+                {
+                    ok = false;
+                    break;
+                }
+            }
+
+            if (ok)
+            {
+                return method.invoke(target, args);
+            }
+        }
+
+        throw new NoSuchMethodException(methodName);
+    }
 }
