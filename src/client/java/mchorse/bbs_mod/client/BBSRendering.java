a04f519f4546f6ddfb4509ae7695fd45de71871b
ElGatoPro300 <catgamer207@gmail.com>
2026-06-15 17:11:13 -0600

    clean up the solution commit code
    Reviewing the changes, literally the solution was just this one line of code

--- src/client/java/mchorse/bbs_mod/client/BBSRendering.java
+++ src/client/java/mchorse/bbs_mod/client/BBSRendering.java
@@ -330,6 +330,8 @@ public class BBSRendering
 
             reassignFramebuffer(framebuffer);
 
+            mc.worldRenderer.onResized(w, h);
+
             framebuffer.beginWrite(true);
         }
         else
