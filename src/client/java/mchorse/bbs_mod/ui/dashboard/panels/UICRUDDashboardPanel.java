284f259dd7917cb883a0fac94706f5be7042162a
ElGatoPro300 <catgamer207@gmail.com>
2026-06-28 17:54:49 -0600

    solucionar tecla N

--- src/client/java/mchorse/bbs_mod/ui/dashboard/panels/UICRUDDashboardPanel.java
+++ src/client/java/mchorse/bbs_mod/ui/dashboard/panels/UICRUDDashboardPanel.java
@@ -2,8 +2,10 @@ package mchorse.bbs_mod.ui.dashboard.panels;
 
 import mchorse.bbs_mod.l10n.keys.IKey;
 import mchorse.bbs_mod.ui.Keys;
+import mchorse.bbs_mod.ui.UIKeys;
 import mchorse.bbs_mod.ui.dashboard.UIDashboard;
 import mchorse.bbs_mod.ui.dashboard.panels.overlay.UICRUDOverlayPanel;
+import mchorse.bbs_mod.ui.dashboard.panels.overlay.UIOpenAssetOverlayPanel;
 import mchorse.bbs_mod.ui.framework.elements.buttons.UIIcon;
 import mchorse.bbs_mod.ui.framework.elements.overlay.UIOverlay;
 import mchorse.bbs_mod.ui.utils.icons.Icons;
@@ -26,7 +28,25 @@ public abstract class UICRUDDashboardPanel extends UISidebarDashboardPanel
 
         this.iconBar.prepend(this.openOverlay);
 
-        this.keys().register(Keys.OPEN_DATA_MANAGER, this.openOverlay::clickItself);
+        this.keys().register(Keys.OPEN_DATA_MANAGER, () ->
+        {
+            if (this.getContext() != null)
+            {
+                java.util.List<UIOpenAssetOverlayPanel> overlays = this.getContext().menu.overlay.getChildren(UIOpenAssetOverlayPanel.class);
+
+                if (!overlays.isEmpty())
+                {
+                    for (UIOpenAssetOverlayPanel overlay : overlays)
+                    {
+                        overlay.close();
+                    }
+                }
+                else
+                {
+                    UIOverlay.addOverlay(this.getContext(), new UIOpenAssetOverlayPanel(UIKeys.RAW_OPEN_ASSET, this.dashboard), 520, 320);
+                }
+            }
+        });
     }
 
     protected abstract UICRUDOverlayPanel createOverlayPanel();
