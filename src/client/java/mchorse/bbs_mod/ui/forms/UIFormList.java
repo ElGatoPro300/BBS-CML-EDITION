25d5619d2008496393504168be404f38f6724069
ElGatoPro300 <catgamer207@gmail.com>
2026-06-30 18:14:09 -0600

    Revert "test 1"
    This reverts commit 3f1e71de4bc44e8a14c2505476cca43de6a02c57.

--- src/client/java/mchorse/bbs_mod/ui/forms/UIFormList.java
+++ src/client/java/mchorse/bbs_mod/ui/forms/UIFormList.java
@@ -2542,13 +2542,6 @@ public class UIFormList extends UIElement
                     this.invalidateCache();
                 }
 
-                if (this.expansionTransition == 0F && this.targetExpansion == 0F && UIFormList.this.expandedCategory != null)
-                {
-                    UIFormList.this.expandedCategory = null;
-                    UIFormList.this.activeExpandedFolder = null;
-                    this.invalidateCache();
-                }
-
                 return;
             }
 
