c1715df67034df38f6f7e48babf9b4db513427cf
ElGatoPro300 <catgamer207@gmail.com>
2026-07-13 22:03:17 -0600

    fix particles

--- src/client/java/mchorse/bbs_mod/BBSModClient.java
+++ src/client/java/mchorse/bbs_mod/BBSModClient.java
@@ -600,10 +600,7 @@ public class BBSModClient implements ClientModInitializer
 
         WorldRenderEvents.AFTER_ENTITIES.register((context) ->
         {
-            if (!BBSRendering.isIrisShadersEnabled())
-            {
-                BBSRendering.renderCoolStuff(context);
-            }
+            BBSRendering.renderCoolStuff(context);
 
             if (BBSRendering.isChromaSkyEnabled())
             {
