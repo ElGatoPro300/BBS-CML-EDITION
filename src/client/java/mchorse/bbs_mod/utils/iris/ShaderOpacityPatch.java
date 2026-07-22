dd20e6adcb3a97acb157722d08b4396618d81c7f
ElGatoPro300 <catgamer207@gmail.com>
2026-07-21 23:06:13 -0600

    fix crash

--- src/client/java/mchorse/bbs_mod/utils/iris/ShaderOpacityPatch.java
+++ src/client/java/mchorse/bbs_mod/utils/iris/ShaderOpacityPatch.java
@@ -544,6 +544,7 @@ public class ShaderOpacityPatch
             }
             else
             {
+                modelViewStack.push();
                 modelViewStack.peek().getPositionMatrix().identity();
                 modelViewStack.peek().getNormalMatrix().identity();
                 RenderSystem.applyModelViewMatrix();
